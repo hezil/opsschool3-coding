945f23b710cc547d336ab3d5e4aef4e43d2778cb
hezil <31658384+hezil@users.noreply.github.com>
2019-07-29 12:13:48 +0300

    Update setup.py

--- home-assignments/grep_click/setup.py
+++ home-assignments/grep_click/setup.py
@@ -1,1 +1,15 @@
-d
+from setuptools import setup
+
+setup(
+    name='grep_click',
+    version='0.1',
+    py_modules=['grep_click'],
+    include_package_data=True,
+    install_requires=[
+        'click',
+    ],
+    entry_points='''
+        [console_scripts]
+        grep_click=grep_click:cli
+    ''',
+)
