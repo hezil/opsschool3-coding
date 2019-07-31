f3871972a158690b7fa3ce232f4a18fc4826a3cb
hezil <31658384+hezil@users.noreply.github.com>
2019-07-31 11:09:46 +0300

    Create setup.py

---
+++ home-assignments/weather_click/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='weather_click',
+    version='0.1',
+    py_modules=['weather_click'],
+    include_package_data=True,
+    install_requires=[
+        'click',
+        'requests',
+    ],
+    entry_points='''
+        [console_scripts]
+        weather_click=weather_click:cli
+    ''',
+)
